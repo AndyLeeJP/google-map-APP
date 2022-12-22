18e3ab36eba2ac923b217e7f2354c4aaba27e1ec
Andy Lee <rize1126jesse@gmail.com>
2022-12-22 17:56:33 +0900

    first deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  GoogleMap,
+  InfoWindow,
+  MarkerF,
+  useLoadScript,
+} from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 
-function App() {
+import "./App.css";
+export default function App() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY
+      ? process.env.REACT_APP_GOOGLE_MAP_API_KEY
+      : "",
+  });
+  console.log(process.env.REACT_APP_GOOGLE_MAP_API_KEY);
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <AppRun />;
+}
+const containerStyle = {
+  height: "100vh",
+  width: "90%",
+};
+
+type currentLocation = {
+  lng: number;
+  lat: number;
+};
+
+interface accessPointType {
+  ssid: string;
+  pos: { lat: number; lng: number };
+  address: string;
+}
+
+type item = {
+  ID: [any, { 識別値: string }];
+  設置地点: {
+    地理座標: {
+      緯度: number;
+      経度: number;
+    };
+    住所: {
+      表記: string;
+    };
+  };
+};
+
+interface selectType {
+  pos: { lat: number; lng: number };
+  address: string;
+}
+
+function AppRun() {
+  const [wifiPlaces, setWifiPlaces] = useState<accessPointType[]>([]);
+  const accessPoints: accessPointType[] = [];
+  const [selectedMarker, setSelectedMarker] = useState<
+    selectType | null | accessPointType
+  >(null);
+  const [currentPlace, setCurrentPlace] = useState<currentLocation>();
+
+  const [myMap, setMyMap] = useState<google.maps.Map>();
+
+  const getApi = async () => {
+    const response = await fetch(
+      "https://api.data.metro.tokyo.lg.jp/v1/WifiAccessPoint?limit=100",
+      {
+        method: "GET",
+
+        headers: {
+          "cache-control": "private",
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }
+    );
+
+    const openData = await response.json();
+
+    openData[0].map((item: item) =>
+      accessPoints.push({
+        ssid: item["ID"][1]["識別値"],
+        pos: {
+          lat: Number(item["設置地点"]["地理座標"]["緯度"]),
+          lng: Number(item["設置地点"]["地理座標"]["経度"]),
+        },
+        address: item["設置地点"]["住所"]["表記"],
+      })
+    );
+    setWifiPlaces(accessPoints);
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentPlace({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
+    setCurrentPlace({
+      lat: 35.6809591,
+      lng: 139.7673068,
+    });
+  }, []);
+
+  console.log("aaaa");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={currentPlace}
+        zoom={17}
+        onLoad={(map: google.maps.Map) => setMyMap(map)}
+      >
+        {wifiPlaces.map((item: accessPointType, i) => (
+          <div key={i}>
+            <MarkerF
+              position={item.pos}
+              onClick={() => {
+                setSelectedMarker(item);
+              }}
+            />
+            {selectedMarker && (
+              <InfoWindow position={selectedMarker.pos}>
+                <div>
+                  <h1>{selectedMarker.address}</h1>
+                  <button onClick={() => setSelectedMarker(null)}>close</button>
+                </div>
+              </InfoWindow>
+            )}
+          </div>
+        ))}
+      </GoogleMap>
+      {/* <button onClick={() => map.panTo({ lat: userLat, lng userLong })}> */}
+      <button onClick={() => myMap?.panTo(currentPlace!)}>現在地に戻る</button>
     </div>
   );
 }
-
-export default App;
